40bad26a9a04792070c58cc33b97292da25e9ee2
shovon61 <=>
2022-09-27 19:09:37 +0600

    some changes in A component

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,15 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { env } from './.env';
+
 export const environment = {
-  production: false
+  production: false,
+  localApiUrl: 'http://localhost:4000',
+  version: env['npm_package_version'] + '-dev',
+  serverUrl: '/api',
+  defaultLanguage: 'en-US',
+  supportedLanguages: ['en-US'],
 };
 
 /*
