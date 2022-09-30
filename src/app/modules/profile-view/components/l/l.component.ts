37ba4803d650859dd0ce4a9ba93a58a9cb493f2e
shovon61 <=>
2022-09-30 15:38:44 +0600

    L component complete

--- src/app/modules/profile-view/components/l/l.component.ts
+++ src/app/modules/profile-view/components/l/l.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-l',
   templateUrl: './l.component.html',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LComponent implements OnInit {
 
-  constructor() { }
+  profile!:FormGroup;
+
+  constructor(private add: FormBuilder) { }
 
   ngOnInit(): void {
+    this.profile=this.add.group({
+      fname:['',Validators.required],
+      lname:['',Validators.required],
+      address:['',Validators.required]
+    })
+  
+   }
+
+  
+  onclick(){
+    console.log(this.profile.value)
   }
 
 }
